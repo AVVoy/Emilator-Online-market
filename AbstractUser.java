bb8ca98fc595fef898f416a6125bc23ebd217e7f
AVVoy <Aleksandr.V.Voynov@yandex.ru>
2024-05-30 19:41:34 +0300

    Create AbstractUser and added data:
    name, id, email, phone, constructor, getters and setters for email, phone, name

---
+++ AbstractUser.java
@@ -0,0 +1,41 @@
+public class AbstractUser {
+
+    private String name;
+
+    private static long count;
+
+    private final long id;
+
+    private String email;
+
+    private String phone;
+    AbstractUser(String name, String phone) {
+        this.name = name;
+        this.phone = phone;
+        this.id = ++count;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
