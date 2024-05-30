d7eee6b2145ab3fe2c78b05411213bc1b6bf6c69
AVVoy <Aleksandr.V.Voynov@yandex.ru>
2024-05-30 20:38:37 +0300

    Added data in Product
    added name, price, description, characteristics and PathToPhoto

--- market/Product.java
+++ market/Product.java
@@ -1,4 +1,20 @@
 package market;
 
+import java.nio.file.Path;
+
 public class Product {
+    private String name;
+
+    private int price;
+
+    private String description;
+
+    private String characteristics;
+
+    private Path pathToPhoto;
+
+    public Product(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
 }
