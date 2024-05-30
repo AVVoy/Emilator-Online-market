deb51d58d66349d2bf3262b6abc490de127d98ed
AVVoy <Aleksandr.V.Voynov@yandex.ru>
2024-05-30 20:51:31 +0300

    Added data in Product
    added setters and getters

--- market/Product.java
+++ market/Product.java
@@ -17,4 +17,44 @@ public class Product {
         this.name = name;
         this.price = price;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCharacteristics() {
+        return characteristics;
+    }
+
+    public void setCharacteristics(String characteristics) {
+        this.characteristics = characteristics;
+    }
+
+    public Path getPathToPhoto() {
+        return pathToPhoto;
+    }
+
+    public void setPathToPhoto(Path pathToPhoto) {
+        this.pathToPhoto = pathToPhoto;
+    }
 }
